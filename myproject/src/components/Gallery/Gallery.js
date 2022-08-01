01629feecb14177f7d21c4b1b488e061bd217487
Atanas Sinapov <sluckata@yahoo.com>
2022-08-01 13:37:13 +0300

    update

--- myproject/src/components/Gallery/Gallery.js
+++ myproject/src/components/Gallery/Gallery.js
@@ -24,7 +24,7 @@ const Gallery = ()=>{
     <div className={styles.main__img__wrapper}>
       <img
         className={styles.main__img}
-        src="assets/15.jpg"
+        src='homepagePath/assets/15.jpg'
         alt="Loading..."
       />
     </div>
