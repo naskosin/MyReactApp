b3885108f4a6b076870e9b99451225b0c7e4c61b
Atanas Sinapov <sluckata@yahoo.com>
2022-08-01 13:53:47 +0300

    update

--- myproject/src/components/Gallery/Gallery.js
+++ myproject/src/components/Gallery/Gallery.js
@@ -30,14 +30,7 @@ const Gallery = ()=>{
     </div>
     <div className={styles.main__div}>
       <h1>Fishing is passion, buying a new bat s the same!</h1>
-      <p>
-        Fish early, and fish late. Many species of fish bite best right at dawn
-        and dusk, in ambient sunlight. During the mid-day hours cloud cover can
-        make for better fishing, and in direct sunlight (especially during the
-        heat of summer) look for areas with shade. Fish often seek out shade
-        when it’s hot and sunny and become more active during the cool hours of
-        the day, just as you or I might.
-      </p>
+   
     </div>
   </article>
   <section className={styles.section__second}>
