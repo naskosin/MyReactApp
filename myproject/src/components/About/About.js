954715251b101775552742b98cd0005cd95cddd0
Atanas Sinapov <sluckata@yahoo.com>
2022-08-01 13:51:23 +0300

    update

--- myproject/src/components/About/About.js
+++ myproject/src/components/About/About.js
@@ -12,17 +12,7 @@ return(
       <h1>About Us</h1>
    
     
-    <p><q>Give a man a fish, and you feed him for a day. Teach a man to fish, and you feed him for a lifetime.</q>
    
-      
-    </p>
-    <p>Today fishing has new morals and ethics, fish is not a food. Fishing is
-        sport and best memories are fishes that we catch, photographing and
-        release .
-      In Biggest catches we are give a fisherman joy to publish his biggest
-      catch, his “once-in-a-lifetime fish” with the story and details. Because
-      memories are our inner fire and faith in future!
-    </p>
   </article>
 </section>
 )
