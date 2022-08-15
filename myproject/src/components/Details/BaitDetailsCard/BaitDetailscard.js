5e92f1291d58895bc70890cda9408cb9cef1ac01
Atanas Sinapov <sluckata@yahoo.com>
2022-08-15 16:09:26 +0300

    Update BaitDetailscard.js

--- myproject/src/components/Details/BaitDetailsCard/BaitDetailscard.js
+++ myproject/src/components/Details/BaitDetailsCard/BaitDetailscard.js
@@ -1,13 +1,11 @@
 import styles from './BaitDetailsCard.module.css';
 import { useAuthContext } from '../../../contexts/AuthContext';
-import { useNavigate } from 'react-router-dom';
-import * as baitService from '../../../services/baitService';
 import { Link } from 'react-router-dom';
 
 
 
 const BaitDetailsCard =({bait,  setShowDeleteDialog})=>{
-const navigate = useNavigate();
+
 const { userInfo } = useAuthContext();
 const token = userInfo.accessToken;
 
