76665519f9146f4c15465424bd3bbfc53fd7021d
Atanas Sinapov <sluckata@yahoo.com>
2023-02-01 10:35:21 +0200

    remove

--- myproject/src/components/Common/GuardedRoutes.js
+++
@@ -1,8 +0,0 @@
-import {Navigate, Outlet} from 'react-router-dom';
-import { useAuthContext } from '../../contexts/AuthContext';
-
-const GuardedRoute = ()=>{
-    const {isLogged} = useAuthContext();
-    return isLogged ? <Outlet/> : <Navigate to='/login'/>
-}
-export default GuardedRoute;
--- myproject/src/components/Common/LoggedGuardedRoute.js
+++
@@ -1,8 +0,0 @@
-import {Navigate, Outlet} from 'react-router-dom';
-import { useAuthContext } from '../../contexts/AuthContext';
-
-const LoggedGuardedRoute = ()=>{
-    const {isLogged} = useAuthContext();
-    return isLogged ?  <Navigate to='/'/>:<Outlet/> 
-}
-export default LoggedGuardedRoute;
